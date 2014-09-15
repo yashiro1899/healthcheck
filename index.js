633fc8e66a4ef01a36cd33ce26a08a78999bfd66
Ice White <yashiro1899@gmail.com>
2014-09-15 16:46:12 +0800

    1410770772

--- index.js
+++ index.js
@@ -41,98 +41,11 @@ var HEALTH_STATE = [
 //     return healthchecks_arr;
 // };
 
-// function check() {
-//     var servers = Object.keys(healthchecks_arr);
-//     var opts = exports.opts;
-
-//     var promises = servers.map(function(s) {
-//         var deferred = getDefer();
-//         var time = new Date();
-//         var hc = healthchecks_arr[s];
-//         if (!hc.since) hc.since = time;
-//         hc.action_time = time;
-
-//         var u = url.format({
-//             protocol: (opts.https ? 'https:' : 'http:'),
-//             host: s,
-//             pathname: (opts.send || "/")
-//         });
-//         u = url.parse(u);
-//         var ended = false;
-
-//         var library = opts.https ? https : http;
-//         var request = library.get({
-//             host: u.hostname,
-//             port: u.port,
-//             agent: false,
-//             path: u.pathname
-//         }, function(response) {
-//             var result = new Buffer('');
-//             if (response.statusCode == 200) {
-//                 response.on("data", function(chunk) {
-//                     result = Buffer.concat([result, chunk]);
-//                 });
-//                 response.on("end", function() {
-//                     if (ended) return null;
-//                     if (opts.expected) {
-//                         if (opts.expected == result.toString()) {
-//                             hc.last_status = HEALTH_STATE[0];
-//                             hc.failcount = 0;
-//                             hc.down = false;
-//                         } else {
-//                             hc.last_status = HEALTH_STATE[3];
-//                             hc.failcount += 1;
-//                             if (hc.failcount >= opts.failcount) hc.down = true;
-//                         }
-//                     } else {
-//                         hc.last_status = HEALTH_STATE[0];
-//                         hc.failcount = 0;
-//                         hc.down = false;
-//                     }
-//                     deferred.resolve(hc);
-//                 });
-//             } else {
-//                 hc.last_status = HEALTH_STATE[6];
-//                 hc.failcount += 1;
-//                 if (hc.failcount >= opts.failcount) hc.down = true;
-//                 deferred.resolve(hc);
-//             }
-//         });
-
-//         request.on('socket', function(socket) {
-//             socket.setTimeout(opts.timeout);
-//             socket.on('timeout', function() {
-//                 ended = true;
-//                 request.abort();
-//                 hc.last_status = HEALTH_STATE[7];
-//                 hc.failcount += 1;
-//                 if (hc.failcount >= opts.failcount) hc.down = true;
-//                 deferred.resolve(hc);
-//             });
-//         });
-
-//         request.on('error', function(error) {
-//             ended = true;
-//             hc.last_status = error.message;
-//             hc.failcount += 1;
-//             if (hc.failcount >= opts.failcount) hc.down = true;
-//             deferred.resolve(hc);
-//         });
-//         return deferred.promise;
-//     });
-
-//     Promise.all(promises).then(function(result) {
-//         result.forEach(function(hc) {
-//             hc.concurrent += 1;
-//         });
-//         if (typeof opts.logger === "function") opts.logger(healthchecks_arr);
-//     });
-// }
 
 exports.HealthCheck = HealthCheck;
 
 function HealthCheck(opts) {
-    var healthchecks_arr = {};
+    this.healthchecks_arr = {};
 
     if (opts && opts.servers && opts.servers.length > 0) {
         if (opts.delay === undefined) opts.delay = 10000;
@@ -140,7 +53,7 @@ function HealthCheck(opts) {
         if (opts.timeout === undefined) opts.timeout = 2000;
 
         opts.servers.forEach(function(s) {
-            healthchecks_arr[s] = {
+            this.healthchecks_arr[s] = {
                 action_time: null,
                 concurrent: 0,
                 down: false,
@@ -155,5 +68,95 @@ function HealthCheck(opts) {
         this.opts = opts;
         this.check();
         setInterval(this.check, opts.delay);
+    } else {
+        opts = null;
     }
 }
+
+HealthCheck.prototype.check = function() {
+    var servers = Object.keys(this.healthchecks_arr);
+    var opts = this.opts;
+
+    var promises = servers.map(function(s) {
+        var deferred = getDefer();
+        var time = new Date();
+        var hc = this.healthchecks_arr[s];
+        if (!hc.since) hc.since = time;
+        hc.action_time = time;
+
+        var u = url.format({
+            protocol: (opts.https ? 'https:' : 'http:'),
+            host: s,
+            pathname: (opts.send || "/")
+        });
+        u = url.parse(u);
+        var ended = false;
+
+        var library = opts.https ? https : http;
+        var request = library.get({
+            host: u.hostname,
+            port: u.port,
+            agent: false,
+            path: u.pathname
+        }, function(response) {
+            var result = new Buffer('');
+            if (response.statusCode == 200) {
+                response.on("data", function(chunk) {
+                    result = Buffer.concat([result, chunk]);
+                });
+                response.on("end", function() {
+                    if (ended) return null;
+                    if (opts.expected) {
+                        if (opts.expected == result.toString()) {
+                            hc.last_status = HEALTH_STATE[0];
+                            hc.failcount = 0;
+                            hc.down = false;
+                        } else {
+                            hc.last_status = HEALTH_STATE[3];
+                            hc.failcount += 1;
+                            if (hc.failcount >= opts.failcount) hc.down = true;
+                        }
+                    } else {
+                        hc.last_status = HEALTH_STATE[0];
+                        hc.failcount = 0;
+                        hc.down = false;
+                    }
+                    deferred.resolve(hc);
+                });
+            } else {
+                hc.last_status = HEALTH_STATE[6];
+                hc.failcount += 1;
+                if (hc.failcount >= opts.failcount) hc.down = true;
+                deferred.resolve(hc);
+            }
+        });
+
+        request.on('socket', function(socket) {
+            socket.setTimeout(opts.timeout);
+            socket.on('timeout', function() {
+                ended = true;
+                request.abort();
+                hc.last_status = HEALTH_STATE[7];
+                hc.failcount += 1;
+                if (hc.failcount >= opts.failcount) hc.down = true;
+                deferred.resolve(hc);
+            });
+        });
+
+        request.on('error', function(error) {
+            ended = true;
+            hc.last_status = error.message;
+            hc.failcount += 1;
+            if (hc.failcount >= opts.failcount) hc.down = true;
+            deferred.resolve(hc);
+        });
+        return deferred.promise;
+    });
+
+    Promise.all(promises).then(function(result) {
+        result.forEach(function(hc) {
+            hc.concurrent += 1;
+        });
+        if (typeof opts.logger === "function") opts.logger(this.healthchecks_arr);
+    });
+};
